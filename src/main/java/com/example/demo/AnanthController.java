1a8cec56cd41f707b421f43a95e392d16800b012
Ananth <vplananth@gmail.com>
2026-01-13 16:44:25 +0530

    i changed

---
+++ src/main/java/com/example/demo/AnanthController.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class AnanthController {
+
+   @GetMapping("/hello") 
+   public String Hello() {
+        return "Hello, Ananth!";
+   } 
+    
+}
--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,9 +6,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
+	public void Hello() {
+		System.out.println("Hello, Ananth!");
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-		System.out.print("Helloooo, World!");
+		
 	}
 
 }
