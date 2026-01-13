9593f11156be0954747bf750ea71797cc7a04efe
Ananth <vplananth@gmail.com>
2026-01-13 17:41:34 +0530

    now changed

--- src/main/java/com/example/demo/AnanthController.java
+++ src/main/java/com/example/demo/AnanthController.java
@@ -1,12 +1,20 @@
 package com.example.demo;
 
-import org.springframework.web.bind.annotation.GetMapping;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class AnanthController {
 
    @GetMapping("/hello") 
    public String Hello() {
-        return "Hello, Ananth!";
+        return "Helloo, Ananth!";
+   } 
+
+   @GetMapping("/login") 
+   public String home() {
+        return "login";
    } 
     
 }
